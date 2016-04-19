69a520d331f1cfecb9e3fad895674bacd119b9b3
Katsuji Matsui <katsuji.matsui@gmail.com>
2016-04-19 18:19:14 +0900

    update for nvim

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+ln -sf ./zshenv ~/.zshenv
+ln -sf ./zshrc ~/.zshrc
+ln -sf ./tmux.conf ~/.tmux.conf
+ln -sf ./inputrc ~/.inputrc
+if [ ! -d ~/.config/nvim ]; then
+  mkdir -p ~/.config/nvim
+fi
+ln -sf ./config/nvim/init.vim ~/.config/nvim/init.vim
