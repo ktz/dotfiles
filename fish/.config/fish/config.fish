b797ee3ccc595c68892a285fc32df9456d5ae2ba
Katsuji Matsui <ktz@iMac.local>
2022-01-07 23:05:21 +0900

    trying out fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -gx SHELL /usr/local/bin//fish
+
+set -gx LANG ja_JP.UTF-8
+set -gx LC_COLLATE "en_US.UTF-8"
+set -gx LC_CTYPE "en_US.UTF-8"
+set -gx LC_MESSAGES "en_US.UTF-8"
+set -gx LC_MONETARY "en_US.UTF-8"
+set -gx LC_NUMERIC "en_US.UTF-8"
+set -gx LC_TIME "en_US.UTF-8"
+
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx CLOUDSDK_PYTHON python
+
+function fish_user_key_bindings
+  bind \cr peco_select_history
+end
+
+set -gx PATH $PATH /usr/local/bin
+
+set -gx PATH ~/.anyenv/bin $PATH
+anyenv init - fish | source
+
+alias tmux='tmux -u'
+alias crontab='EDITOR=nano crontab'
+
