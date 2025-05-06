eed1857af9d26924959401dab5e1eab8ca94f914
Katsuji Matsui <katsuji.matsui@gmail.com>
2025-05-06 13:54:29 +0900

    save

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,9 @@ vim.api.nvim_set_keymap("v", "<leader>c", "gc", { noremap = false, silent = true
 vim.keymap.set("n", "Q", "<nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "q", ":noh<cr>", { noremap = true, silent = true })
 
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>p", [["+p]], { noremap = true, silent = true })
+
 -- vim.keymap.set("n", "<leader>d", "<cmd>Oil --float<CR>")
 vim.keymap.set("n", "<leader>d", function()
 	require("oil").open_float(".")
@@ -17,3 +20,16 @@ end, { desc = "Open Diagnostics in Fload" })
 
 --vim.keymap.set('n', 'Q', '<nop>', { noremap = true, silent = true })
 --vim.keymap.set('n', 'q', ':noh<cr>', { noremap = true, silent = true })
+vim.api.nvim_create_user_command("T", function(opts)
+	vim.cmd("split")
+	vim.cmd("wincmd j")
+	vim.cmd("resize 20")
+	vim.cmd("terminal " .. table.concat(opts.fargs, " "))
+end, { nargs = "*" })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "*",
+	command = "startinsert",
+})
+
+vim.api.nvim_set_keymap("t", "<ESC>", [[<C-\><C-n>]], { noremap = true, silent = true })
---
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	-- config = true,
+	opts = {
+		size = 20,
+		open_mapping = [[<c-t>]],
+	},
+}
